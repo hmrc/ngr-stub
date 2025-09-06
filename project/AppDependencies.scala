a2813c26e399b4d7a8a9ffc0c1df916fe5e44f1f
Yuriy Tumakha <tumakha@gmail.com>
2025-09-06 23:30:39 +0100

    NGR-2480 ngr-stub: Support for POST and PUT stub endpoints has been added

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.18.0"
+  val bootstrapVersion = "10.1.0"
   val hmrcMongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -12,7 +12,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion  % Test,
-    "org.scalamock"          %% "scalamock"                % "6.0.0"           % Test,
+    "org.scalamock"          %% "scalamock"                % "7.4.2"           % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion  % Test
     
   )
