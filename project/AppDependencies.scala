020b164507392da9fdf5ace8fe1ebb07ccb4031e
Ashwini Rudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2025-12-05 09:55:34 +0000

    NGR-3186:Update libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.3.0"
-  val hmrcMongoVersion = "2.10.0"
+  val bootstrapVersion = "10.4.0"
+  val hmrcMongoVersion = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"        % hmrcMongoVersion,
@@ -12,7 +12,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapVersion  % Test,
-    "org.scalamock"          %% "scalamock"                % "7.4.2"           % Test,
+    "org.scalamock"          %% "scalamock"                % "7.5.2"           % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30"  % hmrcMongoVersion  % Test
     
   )
